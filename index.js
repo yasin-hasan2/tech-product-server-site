c3b6c91ee4ff225a0bad0f9c44fb9c193e5a77d4
Yaseen Al Hasan <ya705668@gmail.com>
2023-11-29 23:03:35 -0800

    user post and get

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -33,11 +33,46 @@ async function run() {
 
 
         const productsCollection = client.db("buntydb").collection("products");
+        const usersCollection = client.db('buntydb').collection('users')
+
+
+
 
         app.get("/products", async (req, res) => {
             const result = await productsCollection.find().toArray();
             res.send(result);
         })
+
+        app.get("/products/:id", async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                projection: { name: 1, image: 1, description: 1, price: 1, upvote_count: 1 }
+            }
+
+            const result = await productsCollection.findOne(query, options)
+            res.send(result)
+        })
+
+
+        app.post('/users', async (req, res) => {
+            const users = req.body
+            const result = await usersCollection.insertOne(users)
+            console.log(result)
+            res.send(result)
+        })
+
+        app.get('/users', async (req, res) => {
+            let query = {}
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const users = await usersCollection.find(query).toArray();
+            console.log("this is users", users)
+            res.send(users)
+        })
+
 
 
         // Send a ping to confirm a successful connection

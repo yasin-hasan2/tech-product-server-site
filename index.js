6ad383b973e1537617c8775ea6a8dd183ddbbbd0
Yaseen Al Hasan <ya705668@gmail.com>
2023-12-12 23:06:10 -0800

    tech product server add git

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
+require('dotenv').config()
 const app = express();
 const cors = require('cors');
-require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
 // middleware 
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+}
+app.use(cors(corsConfig));
 app.use(express.json());
 
 
@@ -29,7 +35,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
 
         const productsCollection = client.db("buntydb").collection("products");
@@ -92,7 +98,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
